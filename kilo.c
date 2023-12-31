7d3b90e2b9d0fe1e91cd3fa777b10c0370cf4127
Brandon Durfee <brandon.durfee@gmail.com>
2023-12-31 07:32:32 -0500

    Move file I/O section and add function prototype for editorSetStatusMessage

--- kilo.c
+++ kilo.c
@@ -62,6 +62,8 @@ struct editorConfig {
 
 struct editorConfig E;
 
+/*** prototypes ***/
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -243,6 +245,69 @@ void editorInsertChar(int c) {
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+/*** file i/o ***/
+
+char *editorRowsToString(int *buflen) {
+    int totlen = 0;
+    int j;
+    for (j = 0; j < E.numrows; j++) 
+        totlen += E.row[j].size + 1;
+    *buflen = totlen;
+
+    char *buf = malloc(totlen);
+    char *p = buf;
+    for (j = 0; j < E.numrows; j++) {
+        memcpy(p, E.row[j].chars, E.row[j].size);
+        p += E.row[j].size;
+        *p = '\n';
+        p++;
+    }
+
+    return buf;
+}
+
+void editorOpen(char *filename) {
+    free(E.filename);
+    E.filename = strdup(filename);
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
+    
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;
+        editorAppendRow(line, linelen);
+    }
+    free(line);
+    fclose(fp);
+
+
+}
+
+void editorSave() {
+    if (E.filename == NULL) return;
+
+    int len;
+    char *buf = editorRowsToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    if (fd != -1) {
+        if (ftruncate(fd, len) != -1) {
+            if (write(fd, buf, len) == len) {
+                close(fd);
+                free(buf);
+                editorSetStatusMessage("%d bytes written to disk", len);
+                return;
+            }
+        }
+        close(fd);
+    }
+    free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 /*** append buffer ***/
@@ -486,69 +551,6 @@ void editorProcessKeypress() {
             editorInsertChar(c);
             break;
     }
-}
-
-/*** file i/o ***/
-
-char *editorRowsToString(int *buflen) {
-    int totlen = 0;
-    int j;
-    for (j = 0; j < E.numrows; j++) 
-        totlen += E.row[j].size + 1;
-    *buflen = totlen;
-
-    char *buf = malloc(totlen);
-    char *p = buf;
-    for (j = 0; j < E.numrows; j++) {
-        memcpy(p, E.row[j].chars, E.row[j].size);
-        p += E.row[j].size;
-        *p = '\n';
-        p++;
-    }
-
-    return buf;
-}
-
-void editorOpen(char *filename) {
-    free(E.filename);
-    E.filename = strdup(filename);
-    FILE *fp = fopen(filename, "r");
-    if (!fp) die("fopen");
-    
-    char *line = NULL;
-    size_t linecap = 0;
-    ssize_t linelen;
-    while ((linelen = getline(&line, &linecap, fp)) != -1) {
-        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
-            linelen--;
-        editorAppendRow(line, linelen);
-    }
-    free(line);
-    fclose(fp);
-
-
-}
-
-void editorSave() {
-    if (E.filename == NULL) return;
-
-    int len;
-    char *buf = editorRowsToString(&len);
-
-    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
-    if (fd != -1) {
-        if (ftruncate(fd, len) != -1) {
-            if (write(fd, buf, len) == len) {
-                close(fd);
-                free(buf);
-                editorSetStatusMessage("%d bytes written to disk", len);
-                return;
-            }
-        }
-        close(fd);
-    }
-    free(buf);
-    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 /*** init ***/
