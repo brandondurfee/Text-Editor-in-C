75cc834b0534511ff8bb2720bf2225e2e0c56e0a
Brandon Durfee <brandon.durfee@gmail.com>
2023-12-31 06:39:42 -0500

    Add ability to insert character
    We add editorInsertChar(c) into the default case of editorProcessKeypress. This allows us to call editorInsertChar() whenever we don't have a special character, such as the home key, up arrow, etc.

--- kilo.c
+++ kilo.c
@@ -464,6 +464,10 @@ void editorProcessKeypress() {
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+        
+        default:
+            editorInsertChar(c);
+            break;
     }
 }
 
