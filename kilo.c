3eac5613a13c8a641a8014ae2f23119c98efd8b1
Brandon Durfee <brandon.durfee@gmail.com>
2024-01-03 09:17:03 -0500

    Add more descriptive search prompt in editorFind.

--- kilo.c
+++ kilo.c
@@ -444,7 +444,7 @@ void editorFind() {
     int saved_cy = E.cy;
     int saved_coloff = E.coloff;
     int saved_rowoff = E.rowoff;
-    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
     
     if (query) {
         free(query);
